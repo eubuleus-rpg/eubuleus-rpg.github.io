050df42f3d8441d17bd65c9f75d8feb5f068dfd0
eubuleus-rpg <168216299+eubuleus-rpg@users.noreply.github.com>
2024-04-26 10:46:11 -0700

    fix eruda 1

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() }
+document.createElement('script');
+ script.src="https://cdn.jsdelivr.net/npm/eruda";
+ document.body.appendChild(script);
+script.onload = function () { eruda.init() }
